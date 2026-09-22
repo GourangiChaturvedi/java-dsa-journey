9fb6d7972bf949002781a9295beccfce987e65b7
GOURANGI CHATURVEDI <gourangichaturvedi@gmail.com>
2026-09-22 08:07:16 +0530

    Add Invert_Binary_Tree.java

---
+++ trees/Invert_Binary_Tree.java
@@ -0,0 +1,166 @@
+import java.util.*;
+
+class Node
+
+{
+
+int data;
+
+Node left;
+
+Node right;
+
+Node(int d)
+
+{
+
+data=d;
+
+left=null;
+
+right=null;
+
+}
+
+}
+
+public class Main
+
+{
+
+public static Node BuildTree(int a[])
+
+{
+
+if(a.length==0 || a[0]==-1)
+
+return null;
+
+Node root=new Node(a[0]);
+
+Deque<Node> dq = new ArrayDeque<>();
+
+dq.add(root);
+
+int i=1;
+
+while(!dq.isEmpty() && i<a.length)
+
+{
+
+Node current=dq.poll();
+
+ if(a[i]!=-1)
+
+ {
+
+   current.left=new Node(a[i]);
+
+   dq.add(current.left);
+
+ }
+
+ i++;
+
+ if(i<a.length && a[i]!=-1)
+
+ {
+
+   current.right=new Node(a[i]);
+
+   dq.add(current.right);
+
+ }
+
+ i++;
+
+}
+
+return root;
+
+}
+public static void Swap(Node root)
+    {
+        if(root==null)
+             return;
+        Node left=root.left;
+        Node right=root.right;
+        root.left=right;
+        root.right=left;
+        Swap(root.left);
+        Swap(root.right);
+    }
+public static List<List<Integer>> BFS(Node root)
+
+{
+
+List<List<Integer>> result = new ArrayList<>();
+
+if(root==null)
+
+return result;
+
+Deque<Node> dq = new ArrayDeque<>();
+
+dq.add(root);
+
+while(!dq.isEmpty())
+
+{
+
+int size=dq.size();
+
+ List<Integer> level = new ArrayList<>();
+
+ for(int i=0;i<size;i++)
+
+ {
+
+   Node current=dq.poll();
+
+   level.add(current.data);
+
+   if(current.left!=null)
+
+     dq.add(current.left);
+
+   if(current.right!=null)
+
+     dq.add(current.right);
+
+ }
+
+ result.add(level);
+
+}
+
+return result;
+
+}
+public static void main(String args[])
+
+{
+
+Scanner in = new Scanner(System.in);
+
+System.out.println("Enter the number of nodes");
+
+int n=in.nextInt();
+
+int arr[]=new int[n];
+
+System.out.println("Enter nodes in level order.Enter -1 if node does not exist");
+
+for(int i=0;i<n;i++)
+
+arr[i]=in.nextInt();
+
+Node r=Main.BuildTree(arr);
+Main.Swap(r);
+List<List<Integer>> res=Main.BFS(r);
+System.out.println(res);
+ }
+}
+
+    
+
